d5a57373fcb4f2c304b6828e8b4b66bac670b844
Ahmed Afifi <ahm3afifi@Ahmeds-MacBook-Pro.local>
2018-11-04 01:13:41 +0200

    first commit

---
+++ Window-Shopper-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Window-Shopper-App
+//
+//  Created by Ahmed Afifi on 11/4/18.
+//  Copyright © 2018 Ahmed Afifi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
